f1a1978b600bc4bcd868173a65649de5905d0949
Nina Bednar <nb114@hdm-stuttgart.de>
2021-05-25 11:59:49 +0200

    Bugfix: keine leeren Dateien mehr

--- csv_to_intents.py
+++ csv_to_intents.py
@@ -2,8 +2,6 @@ import csv
 import json
 import os
 
-#TODO: delete empty files
-
 name_list = []
 umlaute = { "Ä":"Ae", 
             "ä":"ae",
@@ -51,68 +49,69 @@ for entry in name_list:
     
     intentdict = {}
     
-    try:
-        with open ("./data/" + intentname + ".json", mode='w') as jsonf:
-            #Antworten
-            intentdict = {
-            
-            'id' : "" ,
-            'name' : intentname,
-            'auto' : True,
-            'contexts' : [],
-            "responses" : [
-            {
-                'resetContexts': False,
-                'action' : intentname,
-                'affectedContexts' : [],
-                'parameters' : [],
-                "messages" : [
-                    {
-                        "type": 0,
-                        "lang" : "de",
-                        "speech" : speech
-                    }
-                ],
-                "defaultResponsePlatforms": {},
-                "speech": []
-            }
-            ],
-            "priority": 500000,
-              "webhookUsed": False,
-              "webhookForSlotFilling": False,
-              "fallbackIntent": False,
-              "events": [],
-              "conditionalResponses": [],
-              "condition": "",
-              "conditionalFollowupEvents": []
-            
-            
-            }
-            json.dump(intentdict, jsonf, indent=2)
-            
-    except FileNotFoundError:
-        continue
-        
-    with open ("./data/" + intentname + "_usersays_de.json", mode='w') as jsonf:
-        #Grundfrage + Training Phrases
-        all_questions_json = []
-        for entry in all_questions:
-            questiondict = {
-            "id": "",
-            "data": [
-                {
-                    "text": entry,
-                    "userDefined": False
-                 }
-            ],
-            "isTemplate": False,
-            "count": 0,
-            "lang": "de",
-            "updated": 0
-            }
-            all_questions_json.append(questiondict)
-            
-        json.dump(all_questions_json, jsonf, indent=2)
-             
+    if intentname:
+        try:
 
-            
+            with open ("./data/" + intentname + ".json", mode='w') as jsonf:
+                #Antworten
+                intentdict = {
+    
+                'id' : "" ,
+                'name' : intentname,
+                'auto' : True,
+                'contexts' : [],
+                "responses" : [
+                {
+                    'resetContexts': False,
+                    'action' : intentname,
+                    'affectedContexts' : [],
+                    'parameters' : [],
+                    "messages" : [
+                        {
+                            "type": 0,
+                            "lang" : "de",
+                            "speech" : speech
+                        }
+                    ],
+                    "defaultResponsePlatforms": {},
+                    "speech": []
+                }
+                ],
+                "priority": 500000,
+                  "webhookUsed": False,
+                  "webhookForSlotFilling": False,
+                  "fallbackIntent": False,
+                  "events": [],
+                  "conditionalResponses": [],
+                  "condition": "",
+                  "conditionalFollowupEvents": []
+    
+    
+                }
+                json.dump(intentdict, jsonf, indent=2)
+    
+        except FileNotFoundError:
+            continue
+
+        with open ("./data/" + intentname + "_usersays_de.json", mode='w') as jsonf:
+            #Grundfrage + Training Phrases
+            all_questions_json = []
+            for entry in all_questions:
+                questiondict = {
+                "id": "",
+                "data": [
+                    {
+                        "text": entry,
+                        "userDefined": False
+                     }
+                ],
+                "isTemplate": False,
+                "count": 0,
+                "lang": "de",
+                "updated": 0
+                }
+                all_questions_json.append(questiondict)
+    
+            json.dump(all_questions_json, jsonf, indent=2)
+     
+    #es entstehen zwei leere Dateien. Da diese nicht gebraucht werden, löschen wir sie
